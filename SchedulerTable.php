331e5d721016d66efa26f20703cfa6c28d0fe115
ajduvanenko <ajduvanenko@bsu.edu>
2015-11-02 10:06:25 -0500

    added scheduler table

---
+++ SchedulerTable.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+th, td {
+    padding: 5px;
+    text-align: left;    
+}
+</style>
+ </head>
+<body>
+<script>
+function shadeCells(){
+ var x = document.getElementById("Fireman").rows.length;
+ var id = document.getElementById("Fireman").rows.item(0).id;
+ document.getElementById("Fireman").rows.item(0).cells.item(0).style.backgroundColor="green";
+ alert(id);
+}
+
+</script>
+
+<h1> Hello, (name/username) (Admin/NonAdmin) </h1>
+<br>
+<h3>Date: xx/xx/20xx (the date would be pulled from the drop down
+that chose what date to be used)</h3>
+<button type="button">Add Time </button>
+<br>
+<br>
+<p id="From1"> From </p> <select>
+  <option value="9am">9am</option>
+  <option value="10am">10am</option>
+  <option value="11am">11am</option>
+  <option value="12pm">12pm</option>
+  <option value="1pm">1pm</option>
+  <option value="2pm">2pm</option>
+  <option value="3pm">3pm</option>
+  <option value="4pm">4pm</option>
+  <option value="5pm">5pm</option>
+</select> 
+
+<p id="To1"> To </p>  <select>
+  <option value="9am">9am</option>
+  <option value="10am">10am</option>
+  <option value="11am">11am</option>
+  <option value="12pm">12pm</option>
+  <option value="1pm">1pm</option>
+  <option value="2pm">2pm</option>
+  <option value="3pm">3pm</option>
+  <option value="4pm">4pm</option>
+  <option value="5pm">5pm</option>
+</select> 
+<br>
+<br>
+<button type="button">Add Time </button>
+<br>
+<br>
+From  <select>
+  <option value="9am">9am</option>
+  <option value="10am">10am</option>
+  <option value="11am">11am</option>
+  <option value="12pm">12pm</option>
+  <option value="1pm">1pm</option>
+  <option value="2pm">2pm</option>
+  <option value="3pm">3pm</option>
+  <option value="4pm">4pm</option>
+  <option value="5pm">5pm</option>
+</select> 
+
+To  <select>
+  <option value="9am">9am</option>
+  <option value="10am">10am</option>
+  <option value="11am">11am</option>
+  <option value="12pm">12pm</option>
+  <option value="1pm">1pm</option>
+  <option value="2pm">2pm</option>
+  <option value="3pm">3pm</option>
+  <option value="4pm">4pm</option>
+  <option value="5pm">5pm</option>
+</select>
+<br>
+<br>
+<br>
+<button type="button" onClick = "shadeCells()">OK</button>
+<button type="button">Reset</button>
+<br>
+<br>
+<?php
+
+function getTimeRow($numberOfHours, $startHour){
+	echo "<tr>";
+	echo"<td>";
+	echo"</td>";
+	for($i=0; $i<$numberOfHours; $i++)
+	{
+		echo "<td>";
+		echo getHour(($startHour+$i));
+		echo "</td>";
+	}
+	echo "</tr>";
+}
+
+function getHour($hour){
+	$am;
+	$hour = $hour%24;
+	$displayHour = $hour;
+	if($hour==0 || $hour ==24)
+	{
+		$displayHour=12;
+		$am=true;
+	}
+	else if($hour>0 && $hour<12)
+	{
+		$displayHour=$hour;
+		$am=true;
+	}
+	else if($hour==12){
+		$displayHour=12;
+		$am=false;
+	}
+	else if($displayHour>12 && $displayHour<24){
+		$displayHour=$hour-12;
+		$am=false;
+	}
+	else{
+		$displayHour=0;
+		$am=false;
+	}
+	$amLabel;
+	if($am){
+		$amLabel="am";
+	}
+	else{
+		$amLabel="pm";
+	}
+	return ($displayHour.":00 ".$amLabel);
+}
+
+echo "<table>";
+getTimeRow(24, 12);
+echo "</table>";
+echo "<table id="Fireman" style="width:100%">";
+echo "<tr id="Header">";
+echo "<th id='0'> </th>";
+echo "<th>9AM</th>";
+echo  "<th id='10'>10AM</th>";
+echo "<th>11AM</th>";
+echo "<th>12PM</th>";
+echo "<th>1PM</th>";
+echo "<th>2PM</th>";
+echo "<th>3PM</th>";
+echo "<th>4PM</th>";
+echo "<th>5PM</th>";
+  </tr>
+
+
+?>
+<table id="Fireman" style="width:100%">
+  <tr id="Header">
+    <th id="0"> </th>
+    <th>9AM</th>
+	<th id="10">10AM</th>
+	<th>11AM</th>
+	<th>12PM</th>
+	<th>1PM</th>
+	<th>2PM</th>
+	<th>3PM</th>
+	<th>4PM</th>
+	<th>5PM</th>
+  </tr>
+  <tr>
+    <td>Bill Gates</td>
+    <td></td>
+    <td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+    <td></td>
+	<td></td>
+	<td></td>
+  </tr>
+  <tr>
+	<td>Alex</td>
+	<td bgcolor="#35eb35"></td>
+    <td></td>
+    <td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+    <td></td>
+	<td></td>
+	<td></td>
+</tr>
+  
+</table>
+
+
+
+</body>
+</html>
