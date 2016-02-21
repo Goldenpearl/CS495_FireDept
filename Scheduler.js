a36b51955b27977dbf3832858fb8743e02c74cbf
Goldenpearl <kblair5@yahoo.com>
2016-02-20 19:12:58 -0500

    Added date navigation

--- Scheduler.js
+++ Scheduler.js
@@ -1,19 +1,61 @@
 	var tableDiv = "tableDiv";
 	var listDiv = "listDiv";
+	var dateDiv = "dateDiv";
+	
+	var dateIdDAILY = 0;
+	var dateIdWEEKLY = 1;
+	var dateIdMONTHLY = 2;
 	
 	function createScheduler(){
+		var timeslots = grabSchedule();
+		var names = getFiremenNames();
+		//createList(timeslots, listDiv);
+		createTable(tableDiv, names);
+		
+		var dateRangeArray = getDateRange(dateIdMONTHLY);
+		var dateIndex = 1;
+		shadeCells(timeslots, names, dateRangeArray[dateIndex]);
+		createDateLabel(dateRangeArray, dateIndex, timeslots, names);
+		
+	}
+	
+	function getDateRange(dateId){
 		var dateGetter = new validDateGetter();
 		var earliestValidDate = dateGetter.getEarliestValidDate();
 		var latestValidDate = dateGetter.getLatestValidDate();
-		var timeslots = grabSchedule();
-		createSchedulerFromTimeslots(timeslots);
+		var dateRangeArray = new Array();
+		var nextStartDate;
+		var nextEndDate = new Date(earliestValidDate.getTime());
+		if(dateId == dateIdDAILY){
+			nextEndDate.setHours(0,0,0,0);
+		}
+		else if(dateId == dateIdWEEKLY){
+			daydiff = nextEndDate.getDate() - nextEndDate.getDay() + (nextEndDate.getDay() == 0 ? -7:0);
+			nextEndDate.setDate(daydiff);
+		}
+		else{
+			nextEndDate.setDate(1);
+		}
+		while(nextEndDate<latestValidDate){
+			nextStartDate = new Date(nextEndDate.getTime());
+			if(dateId == dateIdDAILY){
+				nextEndDate.setDate(nextEndDate.getDate() +1);
+			}
+			else if(dateId == dateIdWEEKLY){
+				nextEndDate.setDate(nextEndDate.getDate() +7);
+			}
+			else{
+				nextEndDate.setMonth(nextEndDate.getMonth()+1);
+			}
+			var dtRange = new DateRange(nextStartDate, nextEndDate);
+			alert(dtRange.getSummary());
+			dateRangeArray.push(dtRange);
+			nextStartDate = new Date(nextStartDate.getTime());
+			nextEndDate = new Date(nextEndDate.getTime());
+		}
+		return dateRangeArray;
 	}
 	
-	function createSchedulerFromTimeslots(timeslotss){
-		createList(timeslotss, listDiv);
-		createTable(timeslotss, tableDiv);
-	}
-
 	function createList(timeslots, divName){
 		var listContents = "";
 		for(var n=0; n<timeslots.length; n++){
@@ -22,9 +64,37 @@
 		document.getElementById(divName).innerHTML = listContents;
 	}
 
-	function createTable(timeslots, divName) {
-			//Get all unique firemen
-			var names = getFiremenNames();
+	function createDateLabel(dateRangeArray, index, timeslots, names){
+		var dateContents = "";
+		var startDate = dateRangeArray[index].startDate;
+		var endDate = dateRangeArray[index].endDate;
+		dateContents += "<button id = 'backButton'> << </button>";
+		//dateContents += "<h3>";
+		dateContents += simpleDate(startDate);
+		dateContents += " - ";
+		dateContents += simpleDate(endDate);
+		//dateContents += "</h3>";
+		dateContents += "<button id = 'forwardButton'> >> </button>";
+		document.getElementById(dateDiv).innerHTML = dateContents;
+		document.getElementById('forwardButton').onclick=function(){changeTableActiveTime(dateRangeArray, index+1, timeslots, names);};
+		document.getElementById('backButton').onclick=function(){changeTableActiveTime(dateRangeArray, index-1, timeslots, names);};
+		if(index<=0){
+			document.getElementById('backButton').disabled=true;
+		}
+		else{
+			document.getElementById('backButton').disabled=false;
+		}
+		if(index>=dateRangeArray.length-1){
+			document.getElementById('forwardButton').disabled=true;
+		}
+		else{
+			document.getElementById('forwardButton').disabled=false;
+		}
+	}
+	
+	function createTable(divName, firefighterNames) {
+			//all unique firemen
+			var names = firefighterNames
 			//Define the header
 			var timeHeader = createTimeHeader();
 			var tablecontents = "";
@@ -52,10 +122,15 @@
             tablecontents += "</table>";
 			//Add the table to the document
 			document.getElementById(tableDiv).innerHTML = tablecontents;
-			//Shade cells
-			shadeCells(timeslots, names);
 	}
 
+	
+	function changeTableActiveTime(dateRangeArray, dateIndex, timeslots, names){
+		if(dateIndex<=dateRangeArray.length-1 && dateIndex>=0)
+		//shadeCells(timeslots, names, dateRangeArray[dateIndex]);
+		createDateLabel(dateRangeArray, dateIndex);
+	}
+	
 	function getFiremenNames(){
 	var names = ["Ash Ketchum", "Misty Bubbles", "Brock Rock"];
 	return names;
@@ -91,7 +166,7 @@
 		return time.getHours();
 	}
 	
-	function shadeCells(timeslots, names){
+	function shadeCells(timeslots, names, dateRange){
 		for(var i=0; i<timeslots.length; i++){	
 			var name = timeslots[i].fireman.getFullName();
 			var startTime = timeslots[i].timeslot;
--- classes.js
+++ classes.js
@@ -77,6 +77,47 @@ function validDateGetter(){
 	}
 }
 
+function DateRange(startDate, endDate){
+	this.startDate =startDate;
+	this.endDate= endDate;
+	
+	this.getSummary = function(){
+		return "Start: "+this.startDate+", End: "+this.endDate;
+	}
+}
+
+function simpleDate(date){
+	return getMonth(date.getMonth())+" "+date.getDate()+", "+date.getFullYear();
+}
+
+function getMonth(month){
+	if(month ==0)
+		return "January";
+	if(month==1)
+		return "February";
+	if(month==2)
+		return "March";
+	if(month==3)
+		return "April";
+	if(month ==4)
+		return "May";
+	if(month==5)
+		return "June";
+	if(month==6)
+		return "July";
+	if(month ==7)
+		return "August";
+	if(month ==8)
+		return "September";
+	if(month == 9)
+		return "October";
+	if(month ==10)
+		return "November";
+	if(month == 11)
+		return "December";
+}
+
+
 function addDays(date, days) {
     var result = new Date(date);
     result.setDate(result.getDate() + days);
