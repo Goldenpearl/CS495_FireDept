5b3f7f7c4017a4681c9c820bd69eb0a7837cc89f
Goldenpearl <kblair5@yahoo.com>
2016-03-16 13:15:58 -0400

    Added a selection for daily

--- Scheduler.js
+++ Scheduler.js
@@ -16,8 +16,13 @@
 		var names = getFiremenNames();
 		createDateSelection(bubbleDiv);
 		//createList(timeslots, listDiv);
-		createTable(tableDiv, names);
 		reloadTable(timeslots, names);
+	}
+	
+	//TODO call database
+	function getFiremenNames(){
+	var names = ["Ash Ketchum", "Misty Bubbles", "Brock Rock"];
+	return names;
 	}
 	
 	//adds radio buttons to html	
@@ -55,53 +60,13 @@
 	
 	//Refreshes header guide, table header, and cell shading
 	function reloadTable(timeslots, names){
-		var dateId = getDateSelectionIndex();
-		var dateRangeArray = getDateRange(dateId);
+		var dateFormatId = getDateSelectionIndex();
+		var dateRangeArray = getDateRange(dateFormatId);
 		var dateIndex = getDateIndex(dateRangeArray);
 		
 		refreshHeaderGuide(dateRangeArray, dateIndex, timeslots, names);
-		refreshTableHeader(dateId, dateRangeArray[dateIndex]);
+		refreshTableCells(tableDiv, getFiremenNames(), dateRangeArray[dateIndex], dateFormatId);
 		refreshCellShading(timeslots, names, dateRangeArray[dateIndex]);
-	}
-	
-	function refreshHeaderGuide(dateRangeArray, dateIndex, timeslots, names){
-		createDateLabel(dateRangeArray, dateIndex, timeslots, names);
-	}
-	
-	function refreshTableHeader(dateId, dateRange){
-		var header = getHeaderOfDateRange(dateId, dateRange);
-		for(var n=0; n<header.length; n++){
-			//alert(header[n].getSummary());
-		}
-	}
-	
-	function refreshCellShading(timeslots, names, dateRange){
-		for(var i=0; i<timeslots.length; i++){	
-			var name = timeslots[i].firefighter.getFullName();
-			var startTime = timeslots[i].timeslot;
-			var nameIndex = getNameIndex(name, names);
-			var timeIndex = getTimeIndex(startTime)
-			if(nameIndex!=-1 && timeIndex !=-1)
-			{
-				document.getElementById('myTable').rows[1+nameIndex].cells[timeIndex+1].setAttribute("bgcolor", "#00FF00");
-
-			}
-		}
-	}
-	
-	//Returns the index of the name in the array, or -1 if name is not found
-	function getNameIndex(name, names){
-		for(var n=0; n<names.length; n++){
-			if(name === names[n]){
-				return n;
-			}
-		}
-		return -1;
-	}
-	
-	function getTimeIndex(date){
-		var time = date.getStartDate();
-		return time.getHours();
 	}
 	
 	//returns value of selected radio button; this value cooresponds to a display format
@@ -163,51 +128,12 @@
 		return dateRangeArray;
 	}
 	
-	function getHeaderOfDateRange(dateId, dateRange){
-		var startDate = new Date(dateRange.startDate.getTime());
-		var endDate = new Date(startDate.getTime());
-		var header = new Array();
-		if(dateId == dateIdDAILY){
-			endDate.setHours(0,0,0,0);
-		}
-		else if(dateId == dateIdWEEKLY){
-			var day = endDate.getDay();
-			var dateDiff =day + (day==0?7:0);
-			endDate.setDate(endDate.getDate()-dateDiff);
-			endDate.setHours(0,0,0,0);
-		}
-		else{
-			endDate.setDate(1);
-			endDate.setHours(0,0,0,0);
-		}
-		while(endDate<dateRange.endDate){
-			startDate=new Date(endDate.getTime());
-			endDate = new Date(startDate.getTime());
-			if(dateId== dateIdDAILY){
-				endDate.setHours(endDate.getHours()+1);
-			}
-			else if(dateId == dateIdWEEKLY){
-				endDate.setHours(endDate.getHours()+4);
-			}
-			else{
-				endDate.setDate(endDate.getDate()+1);
-			}
-			var range = new DateRange(startDate, endDate);
-			header.push(range);
-		}
-		return header;
+	
+	//Refreshes the text above the table
+	function refreshHeaderGuide(dateRangeArray, dateIndex, timeslots, names){
+		createDateLabel(dateRangeArray, dateIndex, timeslots, names);
 	}
 	
-	
-	//Writes the specified schedule in list form
-	function createList(timeslots, divName){
-		var listContents = "";
-		for(var n=0; n<timeslots.length; n++){
-			listContents+=timeslots[n].getSummary()+"<br>";
-		}
-		document.getElementById(divName).innerHTML = listContents;
-	}
-
 	//Creates a label specifying which times are shown, as well as buttons to go back or forward
 	function createDateLabel(dateRangeArray, index, timeslots, names){
 		var dateContents = "";
@@ -238,27 +164,30 @@
 	}
 	
 	//Creates an unshaded table with a row for each firefighter name
-	function createTable(divName, firefighterNames) {
+	function refreshTableCells(divName, firefighterNames, dateRange, dateFormatId) {
 			//all unique firemen
 			var names = firefighterNames
-			//Define the header
-			var timeHeader = createTimeHeader();
+			//Define an array used to fill the header
+			var timeHeaderArray = getTimeHeaderArray(dateRange, dateFormatId);
 			var tablecontents = "";
 			//Define table
 			tablecontents += "<table id = myTable>";
 			tablecontents += "<tr>";
 			tablecontents += "<th>";
 			tablecontents += "</th>";
-			for(var i =0; i< timeHeader.length; i++){
+			for(var i =0; i< timeHeaderArray.length; i++){
 				tablecontents+="<th>";
-				tablecontents+=timeHeader[i] +"</th>";
+				tablecontents+=getDateString(timeHeaderArray[i].startDate, dateFormatId)+"</th>";
+				if(i== timeHeaderArray.length-1){
+					tablecontents+="<th>"+getDateString(timeHeaderArray[i].endDate, dateFormatId)+"</th>";
+				}
 			}
 			tablecontents += "</tr>"
 			for(var i=0; i<names.length; i++){ //create a row for each firefighter
 				tablecontents+="<tr>";
 				tablecontents+="<th>";
 				tablecontents+= names[i];
-				for(var j=0; j<timeHeader.length; j++){ //create a cell for each column header
+				for(var j=0; j<timeHeaderArray.length+1; j++){ //create a cell for each column header
 					tablecontents+= "<td class=table-cell>";
 					tablecontents+= "</td>";
 				}
@@ -269,22 +198,43 @@
 			//Add the table to the document
 			document.getElementById(divName).innerHTML = tablecontents;
 	}
-
-	//Called to update the time range of the schedule (called when you push back or forward)
-	function changeTableActiveTime(dateRangeArray, dateIndex, timeslots, names){
-		if(dateIndex<=dateRangeArray.length-1 && dateIndex>=0)
-		//shadeCells(timeslots, names, dateRangeArray[dateIndex]);
-		createDateLabel(dateRangeArray, dateIndex);
-	}
 	
-	//TODO call database
-	function getFiremenNames(){
-	var names = ["Ash Ketchum", "Misty Bubbles", "Brock Rock"];
-	return names;
-	}
+	function getTimeHeaderArray(dateRange, dateFormatId){
+		var startDate = new Date(dateRange.startDate.getTime());
+		var endDate = new Date(startDate.getTime());
+		var header = new Array();
+		if(dateFormatId == dateIdDAILY){
+			endDate.setHours(0,0,0,0);
+		}
+		else if(dateFormatId == dateIdWEEKLY){
+			var day = endDate.getDay();
+			var dateDiff =day + (day==0?7:0);
+			endDate.setDate(endDate.getDate()-dateDiff);
+			endDate.setHours(0,0,0,0);
+		}
+		else{
+			endDate.setDate(1);
+			endDate.setHours(0,0,0,0);
+		}
+		while(endDate<dateRange.endDate){
+			startDate=new Date(endDate.getTime());
+			endDate = new Date(startDate.getTime());
+			if(dateFormatId== dateIdDAILY){
+				endDate.setHours(endDate.getHours()+1);
+			}
+			else if(dateFormatId == dateIdWEEKLY){
+				endDate.setHours(endDate.getHours()+4);
+			}
+			else{
+				endDate.setDate(endDate.getDate()+1);
+			}
+			var range = new DateRange(startDate, endDate);
+			header.push(range);
+		}
+		return header;
 	
-	function createTimeHeader(){
-	var timeHeader = new Array();
+		/*
+		var timeHeader = new Array();
 			timeHeader[0]="12:00 am";
 			for(var i=1; i<12; i++){
 				var str = i+":00 am";
@@ -296,8 +246,83 @@
 				timeHeader[12+i]=str;
 			} 		
 			timeHeader[24] = "12:00 am";
-			return timeHeader;
+			return timeHeader;*/
 	}
+	
+	function getDateString(date, dateFormatId){
+		if(dateFormatId==dateIdDAILY){
+			var hours = date.getHours();
+			var dateString = "";
+			var m = "am";
+			if(hours>=12){
+				m="pm";
+			}
+			hours = hours%12;
+			if(hours == 0){
+				hours = 12;
+			}
+			dateString+=hours;
+			dateString += ":00 ";
+			dateString+=m;
+			return dateString;
+			
+		}
+		else if (dateFormatId == dateIdWEEKLY){
+			
+		}
+		else{
+			
+		}
+		return "gg";
+	}
+	
+	//Shades cells that are scheduled
+	function refreshCellShading(timeslots, names, dateRange){
+		for(var i=0; i<timeslots.length; i++){	
+			var name = timeslots[i].firefighter.getFullName();
+			var startTime = timeslots[i].timeslot;
+			var nameIndex = getNameIndex(name, names);
+			var timeIndex = getTimeIndex(startTime)
+			if(nameIndex!=-1 && timeIndex !=-1)
+			{
+				document.getElementById('myTable').rows[1+nameIndex].cells[timeIndex+1].setAttribute("bgcolor", "#00FF00");
+
+			}
+		}
+	}
+	
+	//Returns the index of the name in the array, or -1 if name is not found
+	function getNameIndex(name, names){
+		for(var n=0; n<names.length; n++){
+			if(name === names[n]){
+				return n;
+			}
+		}
+		return -1;
+	}
+	
+	function getTimeIndex(date){
+		var time = date.getStartDate();
+		return time.getHours();
+	}
+	
+	//Writes the specified schedule in list form
+	function createList(timeslots, divName){
+		var listContents = "";
+		for(var n=0; n<timeslots.length; n++){
+			listContents+=timeslots[n].getSummary()+"<br>";
+		}
+		document.getElementById(divName).innerHTML = listContents;
+	}
+
+	//Called to update the time range of the schedule (called when you push back or forward)
+	function changeTableActiveTime(dateRangeArray, dateIndex, timeslots, names){
+		if(dateIndex<=dateRangeArray.length-1 && dateIndex>=0)
+		//shadeCells(timeslots, names, dateRangeArray[dateIndex]);
+		createDateLabel(dateRangeArray, dateIndex);
+	}
+	
+	
 
 	
 	
