423a140a18e8bb753bc9a396a71d1b218d9e7cac
Goldenpearl <kblair5@yahoo.com>
2016-04-15 11:07:06 -0400

    Dynamic

--- alexCalendar.js
+++ alexCalendar.js
@@ -4,22 +4,41 @@ document.write("Hi");
 
 }
 
-function drawCalendar() {
+function drawCalendar(date) {
 	var tableString = "";
 	tableString += "<p class='text-center'>";
 	tableString += "<button> &lt </button>";
-	tableString += "<strong> Feb </strong>";
+	tableString += "<strong> "
+	tableString += getMonthName(date);
+	tableString += " </strong>";
 	//tableString += "<p class='text-center'> <Feb </p>";
 	tableString += "<button> &gt </button>";
 	tableString += "</p>";
 	tableString += "<table class='table table-bordered'>";
 	tableString += "<th> Sunday </th> <th> Monday </th> <th> Tuesday </th> <th> Wednesday </th> <th> Thursday </th> <th> Friday </th> <th> Saturday </th>";
+	var monthHasBegun = false;
+	var numberOfDaysInPreviousMonth = getNumberOfDaysInMonth(date);
+	var weekdayOfFirstMonth = getFirstOfMonth(date);
+	var numberOfDaysInMonth = getNumberOfDaysInMonth(date);
+	var monthHasEnded = false;
+	var currentNumber = numberOfDaysInPreviousMonth-weekdayOfFirstMonth;
 	for (var r = 0; r < 6; r++) {
 		tableString += "<tr>";
 		for (var i = 0; i < 7; i++) {
-			var number = i + r * 7;
+			if(!monthHasBegun)
+			{
+				if(i>=weekdayOfFirstMonth){
+					monthHasBegun=true;
+					currentNumber=0;
+				}
+			}
+			currentNumber++;
+			if(currentNumber>numberOfDaysInMonth){
+				currentNumber=1;
+				monthHasEnded=true;
+			}
 			tableString += "<td style='width:5%' class='table-active'>";
-			tableString += number;
+			tableString += currentNumber;
 			tableString += "</td>";
 		}
 	tableString += "</tr>";
@@ -27,4 +46,29 @@ function drawCalendar() {
 	tableString += "</table>";
 	
 	document.getElementById("myTable").innerHTML = tableString;
+}
+
+
+function drawCurrentCalendar(){
+	drawCalendar(new Date());
+}
+
+/*returns a number from 0-6 corresponding to a weekday. The weekday cooresponds to the 1st day of the month.*/
+function getFirstOfMonth(date){
+	var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+	var weekday = firstDay.getDay();
+	return weekday;
+	
+}
+/*returns a number from 1-31 which corresponds to the total number of days in the given month.*/
+function getNumberOfDaysInMonth(date){
+	var lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0);
+	var number = lastDay.getDate();
+	return number;
+}
+
+function getMonthName(date){
+	var monthNames = ["January", "February", "March", "April", "May", "June",
+		"July", "August", "September", "October", "November", "December"];
+	return monthNames[date.getMonth()];
 }
